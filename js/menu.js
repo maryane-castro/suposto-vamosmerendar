a9a0cd901492e90ea9b7207fcf9981ec9a785e92
maryane-castro <maryane.castro993@gmail.com>
2023-03-18 20:17:36 -0300

    .

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ const menuToggle = menuSection.querySelector(".menu-toggle");
 
 menuToggle.addEventListener("click", () => {
 
-    document.body.style.overflow = visul ? "hidden" : "initial"
+    
     menuSection.classList.toggle("on", visul);
     visul = !visul;
 })
